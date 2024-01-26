d5158e190229ca15d3927eb6154f0714f2da6887
Diego Lima <83326137+diegofdsl@users.noreply.github.com>
2024-01-26 16:13:09 -0300

    Dicionário em Python

--- criando_dicionario.py
+++ criando_dicionario.py
@@ -1,15 +1,30 @@
-pessoa = {
-    'nome':'Luiz Otavio',
-    'sobrenome':'Miranda',
-    'idade': 18,
-    'altura': 1.80,
-    'endereço': [{'rua': 'tal tal', 'numero':123},{'rua':'outra rua','numero':321}]
+# pessoa = {
+#     'nome':'Luiz Otavio',
+#     'sobrenome':'Miranda',
+#     'idade': 18,
+#     'altura': 1.80,
+#     'endereço': [{'rua': 'tal tal', 'numero':123},{'rua':'outra rua','numero':321}]
+#
+# }
+# # pessoa = dict(nome = 'Luiz Otávio',sobrenome ='Miranda'
+# print(pessoa['nome'])
+# print(pessoa['endereço'])
+# print()
+#
+# for a in pessoa:
+#     print(a, pessoa[a])
+# pessoa ={}
+#
+# chave = 'nome_completo'
+# pessoa[chave] = ' Diego '
+# print(pessoa[chave])
+# print(pessoa)
+# # Pra verificar se a chave existe
+# print(pessoa.get('sobrenome','Nao existe essa chave'))
+# if pessoa.get('sobrenome') is None:
+#     print('Não Existe')
+# else:
+#     print('Existe')
 
-}
-# pessoa = dict(nome = 'Luiz Otávio',sobrenome ='Miranda'
-print(pessoa['nome'])
-print(pessoa['endereço'])
-print()
 
-for a in pessoa:
-    print(a, pessoa[a])
+
