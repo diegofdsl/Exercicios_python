b7b93f7dd5aa6aac0a92fb67b0f890fdf7fc3901
Diego Lima <83326137+diegofdsl@users.noreply.github.com>
2024-01-26 16:31:56 -0300

    Dicionário em Python

--- criando_dicionario.py
+++ criando_dicionario.py
@@ -5,18 +5,41 @@ pessoa = {
     'altura': 1.80,
     'endereço': [{'rua': 'tal tal', 'numero':123},{'rua':'outra rua','numero':321}]}
 
-#Quantas chaves
-print(len(pessoa))
-#Mostrar as chaves
-print(pessoa.keys())
-#mostrar valores
-print(pessoa.values())
-#mostrar chaves e valores correspondentes
-print(pessoa.items())
+# #Quantas chaves
+# print(len(pessoa))
+# #Mostrar as chaves
+# print(pessoa.keys())
+# #mostrar valores
+# print(pessoa.values())
+# #mostrar chaves e valores correspondentes
+# print(pessoa.items())
+# # Pra verificar se a chave existe
+# print(pessoa.get('sobrenome','Nao existe essa chave'))
+# if pessoa.get('sobrenome') is None:
+#     print('Não Existe')
+# else:
+#     print('Existe')
 
+#shallow copy
 
+d1={'c1':1,
+    'c2':2
+}
 
+print(d1)
+d2 = d1
+d2['c1']= 1000
+print(d1)
+print(d2)
 
+#metodo copy
+e1 = {'e1':1,
+      'e2':2,
+      }
+e2=e1.copy()
+e2['e2']=10
+print(e1)
+print(e2)
 
 
 # }
@@ -33,12 +56,7 @@ print(pessoa.items())
 # pessoa[chave] = ' Diego '
 # print(pessoa[chave])
 # print(pessoa)
-# # Pra verificar se a chave existe
-# print(pessoa.get('sobrenome','Nao existe essa chave'))
-# if pessoa.get('sobrenome') is None:
-#     print('Não Existe')
-# else:
-#     print('Existe')
+
 
 
 
